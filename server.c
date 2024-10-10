5ffb4688b159deeb8349a39047ea33f7d6b19637
EshwarChawda <eshwarchawda08@gmail.com>
2024-10-11 00:08:56 +0530

    Accepting login credentials from client

--- server.c
+++ server.c
@@ -1,37 +1,95 @@
+#include "utils.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 
-#define PORT 65000
+#define PORT 65001
 #define BUFFER_SIZE 1024
 
+char buffer[BUFFER_SIZE];
+
+void GetCredentialsFromClient(int socketfd){
+	send(socketfd, loginIdLabel, loginIdLabelLength, 0);
+
+	memset(buffer, 0, BUFFER_SIZE);
+	read(socketfd, buffer, BUFFER_SIZE);
+
+	char loginId[100];
+	char password[100];
+
+	strcpy(loginId, buffer);
+
+	send(socketfd, passwordLabel, passwordLabelLength, 0);
+
+	memset(buffer, 0, BUFFER_SIZE);
+	read(socketfd, buffer, BUFFER_SIZE);
+
+	strcpy(password, buffer);
+
+	memset(buffer, 0, BUFFER_SIZE);
+}
+
+int SendWelcomeMenu(int socketfd) {
+	// Sending the Predefined Welcome Menu to the client
+	send(socketfd, clientMenu, clientMenuLength, 0);
+
+	// Waiting for the response from the client
+	int responseValue;
+
+	read(socketfd, &responseValue, sizeof(responseValue));
+
+	responseValue = ntohl(responseValue);
+
+	// Responding to the client as per the Selected Menu Item
+	switch (responseValue)
+	{
+		case 1: GetCredentialsFromClient(socketfd);
+			break;
+		
+		case 2:
+			break;
+		
+		case 3:
+			break;
+		
+		case 4:
+			break;
+		
+		case 5: 
+			break;
+		
+		default: SendWelcomeMenu(socketfd);
+			break;
+	}
+
+	return 0;
+}
+
 int main() {
+	init();
+
     int server_fd, new_socket;
     struct sockaddr_in address;
-    int opt = 1;
     int addrlen = sizeof(address);
-    char buffer[BUFFER_SIZE];
-    const char *ip_address = "127.0.0.1";
-	int val;
-	char user_ID[100];
-	int pass;
 
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
 
     address.sin_family = AF_INET;
     address.sin_port = htons(PORT);
 
-	inet_pton(AF_INET, ip_address, &address.sin_addr);
+	inet_pton(AF_INET, serveripAddress, &address.sin_addr);
 
     bind(server_fd, (struct sockaddr *)&address, sizeof(address));
 
     listen(server_fd, 3);
 
-    printf("Waiting for connections on %s:%d...\n", ip_address, PORT);
+	signal(SIGCHLD, SIG_IGN);
 
-	const char *firstMenu = "Enter log-in type: \n 1. Customer \n 2. Bank Employee \n 3. Manager \n 4. Administrator \n 5. Exit \n Enter your choice : ";
+    printf("Waiting for connections on %s:%d...\n", serveripAddress, PORT);
 
 	while(1) {
 		new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
@@ -39,19 +97,10 @@ int main() {
 		int processId = fork();
 
 		if(processId == 0) {
-			while(1) {
-				send(new_socket, firstMenu, strlen(firstMenu), 0);
-
-				read(new_socket, &val, sizeof(val));
-
-				if(ntohl(val)==5)
-					break;
-
-				printf("Menu item selected : %d\n", ntohl(val));
-			}
-			printf("Client has Terminated");
+			SendWelcomeMenu(new_socket);
 			close(new_socket);
-			break;
+			close(server_fd);
+			exit(0);
 		}
 		else {
 			close(new_socket);
---
+++ utils.h
@@ -0,0 +1,16 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+extern const char *serveripAddress;
+
+extern const char *clientMenu;
+extern const char *loginIdLabel;
+extern const char *passwordLabel;
+
+extern int clientMenuLength;
+extern int loginIdLabelLength;
+extern int passwordLabelLength;
+
+void init();
+
+#endif 
